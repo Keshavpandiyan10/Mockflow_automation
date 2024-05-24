c9806216597909057424e9abc38cf8cbfcd8263c
Keshav Pandiyan <keshavpandiyan@192.168.1.42>
2024-05-24 10:13:42 +0530

    Updated code

--- src/main/java/mockflow/CreatePage.java
+++ src/main/java/mockflow/CreatePage.java
@@ -23,17 +23,18 @@ public class CreatePage {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		driver.get("https://mockflow.com/");
 		driver.manage().window().maximize();
-		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
 		
+
+
 		//Scroll to bottom and back to top       		
-        WebElement BottomElement = driver.findElement(By.xpath("//div[contains(@class, 'mf_type--mini mr-3 d-lg-inline mf_copyright_link')]"));	
-        js.executeScript("arguments[0].scrollIntoView();", BottomElement);      
-        System.out.println(driver.findElement(By.xpath("//div[contains(@class, 'mf_type--mini mr-3 d-lg-inline mf_copyright_link')]")).getText());        
-        Thread.sleep(3000);
-        WebElement TopElement = driver.findElement(By.xpath("//div[@id='MockflowNavBar']"));
-        js.executeScript("arguments[0].scrollIntoView();", TopElement);
-        System.out.println(driver.findElement(By.xpath("//div[@id='MockflowNavBar']")).getText());
-        Thread.sleep(3000);
+		WebElement BottomElement = driver.findElement(By.xpath("//div[contains(@class, 'mf_type--mini mr-3 d-lg-inline mf_copyright_link')]"));	
+		js.executeScript("arguments[0].scrollIntoView();", BottomElement);      
+		System.out.println(driver.findElement(By.xpath("//div[contains(@class, 'mf_type--mini mr-3 d-lg-inline mf_copyright_link')]")).getText());        
+		Thread.sleep(3000);
+		WebElement TopElement = driver.findElement(By.xpath("//div[@id='MockflowNavBar']"));
+		js.executeScript("arguments[0].scrollIntoView();", TopElement);
+		System.out.println(driver.findElement(By.xpath("//div[@id='MockflowNavBar']")).getText());
+		Thread.sleep(3000);
 
 		//Login to account
 		driver.findElement(By.xpath("//button[@class='btn']")).click();
@@ -42,49 +43,50 @@ public class CreatePage {
 		driver.findElement(By.xpath("//input[@name='logpassword']")).sendKeys(getPassword());
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@name='submit']")).click();
-		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.mfUpgradePromo")));
-		
-		
-		
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[text()='Create New'])[1]")));
+
+
+
 		//Create a DarkMode UI
 		driver.findElement(By.xpath("(//span[text()='Create New'])[1]")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@data-id='webapp']")).click();
 		Thread.sleep(3000);
 		WebElement DarkUI = driver.findElement(By.xpath("//div[@data-name='Darkmode UI']"));   
-        js.executeScript("arguments[0].scrollIntoView();", DarkUI);
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//div[@data-name='Darkmode UI']")).click();
-        Thread.sleep(3000);       
-        driver.findElement(By.xpath("//input[@id='mfUnityCreateProjectTitle']")).sendKeys("Wireframe - For automation purpose");       
-        driver.findElement(By.xpath("//button[@id='mfNewProjectModalBtn']")).click();        
-        Thread.sleep(5000); 
-        //drag and drop
-    	WebElement From=driver.findElement(By.xpath("(//div[@class='mfCategoryItemsWrapper']/div)[1]"));	    
-      	WebElement To=driver.findElement(By.xpath("//div[@class='ui-droppable']"));					
-    	Actions act=new Actions(driver);					
-    	act.dragAndDrop(From, To).build().perform();	
-    	Thread.sleep(2000);
-    	
-    	driver.findElement(By.xpath("//div[@id='mfNavPagesBtn']")).click();
-    	Thread.sleep(3000);
-    	driver.findElement(By.xpath("//span[@id='mfPageAddBtn']")).click();
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//span[@id='mfPageAddFolderBtn']")).click();
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//span[@id='mfPageDupBtn']")).click();
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//span[@id='mfPageEditBtn']")).click();
-    	Thread.sleep(3000);
-    	driver.findElement(By.xpath("//input[@id='mfAddPageTitle']")).clear();
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//input[@id='mfAddPageTitle']")).sendKeys("Page dup");
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//a[@id='mfAddPageModalBtn']")).click();
-    	Thread.sleep(2000);
-    	driver.findElement(By.xpath("//span[@id='mfPageDelBtn']")).click();
-    	Thread.sleep(3000);
-    	driver.findElement(By.xpath("//button[@class='btn btn-primary bootbox-accept']")).click();
+		js.executeScript("arguments[0].scrollIntoView();", DarkUI);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@data-name='Darkmode UI']")).click();
+		Thread.sleep(3000);       
+		driver.findElement(By.xpath("//input[@id='mfUnityCreateProjectTitle']")).sendKeys("Wireframe - For automation purpose");       
+		driver.findElement(By.xpath("//button[@id='mfNewProjectModalBtn']")).click();        
+		Thread.sleep(5000); 
+		//drag and drop
+		WebElement From=driver.findElement(By.xpath("(//div[@class='mfCategoryItemsWrapper']/div)[1]"));	    
+		WebElement To=driver.findElement(By.xpath("//div[@class='ui-droppable']"));					
+		Actions act=new Actions(driver);					
+		act.dragAndDrop(From, To).build().perform();	
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//div[@id='mfNavPagesBtn']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//span[@id='mfPageAddBtn']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@id='mfPageAddFolderBtn']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@id='mfPageDupBtn']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@id='mfPageEditBtn']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@id='mfAddPageTitle']")).clear();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='mfAddPageTitle']")).sendKeys("Page dup");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@id='mfAddPageModalBtn']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@id='mfPageDelBtn']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[@class='btn btn-primary bootbox-accept']")).click();
 	}
 
 }
